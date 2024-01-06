222dc5f4662dad905b7fcbf0d46d3ba7a11557e5
ahtisham521 <ahtisham.mlk123@gmail.com>
2024-01-06 21:38:07 +0500

    initial commit

---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/app/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('hello world test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.byType(Container), findsOneWidget);
+    expect(find.byType(Center), findsOneWidget);
+    expect(find.text('Hello World'), findsOneWidget);
+  });
+}
